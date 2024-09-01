1b33b63ae0148d0e83cba596169e762ddc3193b3
Josh McDannel <mcdannel@gmail.com>
2024-09-01 13:39:27 -0600

    feat: Update dependencies and add Vuestic UI library
    This commit updates the dependencies in the `package.json` and `pnpm-lock.yaml` files to include the latest stable versions of `@vueuse/core`, `@vueuse/firebase`, `vue`, `vuefire`, and `vuestic-ui`. It also adds the `vuestic-ui` library as a new dependency. The `resolutions` field in `package.json` is also updated to include an alias for the `@` symbol, pointing to the `./src` directory. This will make it easier to import files from the `src` directory using the `@` alias.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,4 @@
 /** @type {import('tailwindcss').Config} */
-import daisyui from 'daisyui'
 export default {
   content: [
     "./index.html",
@@ -8,22 +7,5 @@ export default {
   theme: {
     extend: {},
   },
-  plugins: [daisyui],
-  daisyui: {
-    themes: [
-      {
-        mytheme: {
-          "primary": "rgb(14, 165, 233)",
-          "secondary": "#7818e5",
-          "accent": "#10b981",
-          "neutral": "#2a323c",
-          "base-100": "#1f2937",
-          "info": "#3abff8",
-          "success": "#36d399",
-          "warning": "#fbbd23",
-          "error": "#f87272",
-        },
-      },
-    ],
-  },
+  plugins: [],
 }
